f551bebb872b20625aa9b0886bb6f1ef1c660deb
Paz goldberg <pazgold31@gmail.com>
2022-09-25 15:28:37 +0300

    added pause and continue hotkeys for clicker

--- common/hotkeys.py
+++ common/hotkeys.py
@@ -1,16 +1,12 @@
+from typing import Callable
+
 import keyboard
-from PySimpleGUI import Input
-from ahk import AHK
 
 
 class Hotkeys:
-    def __init__(self, ahk: AHK, x_pos: Input, y_pos: Input):
-        self._ahk = ahk
-        self._x_pos = x_pos
-        self._y_pos = y_pos
-        keyboard.add_hotkey('ctrl + shift + r', self._record_tower)
+    def __init__(self):
+        pass
 
-    def _record_tower(self):
-        x, y = self._ahk.mouse_position
-        self._x_pos.update(x)
-        self._y_pos.update(y)
+    @staticmethod
+    def add_hotkey(keys: str, callback: Callable):
+        keyboard.add_hotkey(keys, callback)
