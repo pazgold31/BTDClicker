6210355445d2052c8012d6251cd269a5b7fa1090
Paz goldberg <pazgold31@gmail.com>
2022-12-01 20:24:39 +0200

    added command line argument to load script when opening the maker

--- script_maker/make_script.py
+++ script_maker/make_script.py
@@ -1,8 +1,20 @@
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
 from script_maker.gui.gui_class import GuiClass
 
 
+def parse_args() -> Namespace:
+    argument_parser = ArgumentParser("BTD6 script maker")
+    argument_parser.add_argument("-i", "--import-script", type=Path, required=False, default=None)
+    return argument_parser.parse_args()
+
+
 def main():
+    args = parse_args()
     gui_class = GuiClass()
+    if args.import_script:
+        gui_class.import_script(script_path=args.import_script)
     gui_class.run()
 
 
